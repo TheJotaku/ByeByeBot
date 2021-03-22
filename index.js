1f0c40aca1d8d9aa6360ea7bf1d0693b12142fb0
MrJotaku <joemelito1193@gmail.com>
2021-03-22 18:54:53 -0400

    Update 1.1
    - Removed Commands
    - Added Beatles reference
    - Added CodeSnippets.txt (just notes)
    -  Update HTML to reflect notes

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 // TODO: 
 // getting afk channel (look at roles and permissions within channels)
 // get users in a voice channel so message does not display if there is only one person in the voice channel 
- 
+// HOST Static webpage
 
 const Discord = require('discord.js')
-const ytdl =  require('ytdl-core');
+
 const client = new Discord.Client();
 const prefix = '!!';
 const byeByeByeTrack = "./Audio/ByeByeBye.mp3";
-const drDD = "https://www.youtube.com/watch?v=aYekOTLmOLs&t=1s"; 
-var audioFile = "";
-
-//const path = require('path');
+const path = require('path');
+const byeByeBot = new Discord.MessageAttachment(path.join(__dirname,"./Pictures/byebye.png"));
 //const botCommandsFile = new Discord.MessageAttachment(path.join(__dirname,"./ConfigFiles/ByeByeBotCommands.html"));
 
 client.once('ready', () => {
   console.log('ByeByeBot Is Now Online');
-  client.user.setPresence({ activity: { name: '!TellByeBye' }, status: 'available' })
+  client.user.setPresence({ activity: { name: '!!TellByeBye' }, status: 'available' })
   .then(console.log)
   .catch(console.error);
 });
@@ -49,37 +47,22 @@ client.on('message',async message =>{
           break;
         
         case 'thebyebyebot':
-          message.channel.send("Don't Think It! Don't Say It!");
+          message.channel.send(messageUserID+ " Don't Think It! Don't Say It!", {files: [byeByeBot]});
           break;
         
         case 'byebyebye':
-          audioFile = byeByeByeTrack;
-          connection.play(audioFile);
+          const connection = await message.member.voice.channel.join();
+          connection.play(byeByeByeTrack);
           message.channel.send(messageUserID +" You may hate me but it ain't no lie Baby bye bye bye");
-          break;
-         
-        case 'gillette': 
-          audioFile = drDD;
-          connection.play(ytdl(audioFile, { filter: 'audioonly' }));  
-          message.channel.send(messageUserID +" RAUUUUUUUUULLLLLLL");
           break;
         
         case 'kick':
           await message.member.voice.channel.leave();
           break; 
-          
-        case 'pause':
-          connection.play(audioFile).pause(true);
-          break;
 
-        case 'resume':
-          //TODO: youtube will not resume? :(
-          connection.play(audioFile).resume();
-          break;
-
-        case 'user-info':
-          message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-          break;        
+        case 'goodbye':
+          message.channel.send(messageUserID +" I don't know why you say hello, I say goodbye!");
+          break; 
     }
 
 });
