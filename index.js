8eadc03dd48e74ae2fb8cae477fe8f6c2f57f602
MrJotaku <joemelito1193@gmail.com>
2021-03-21 22:13:51 -0400

    Play Music
    ByeBye now plays local music file byebyebye
    you can also kick the bot from the current voice channel

--- index.js
+++ index.js
@@ -8,8 +8,9 @@
 const Discord = require('discord.js')
 const client = new Discord.Client();
 const prefix = '!';
-//const path = require('path');
-//const botCommandsFile = new Discord.MessageAttachment(path.join(__dirname,"./ConfigFiles/ByeByeBotCommands.html"));
+const path = require('path');
+const byeByeByeTrack = "./Audio/ByeByeBye.mp3";
+const botCommandsFile = new Discord.MessageAttachment(path.join(__dirname,"./ConfigFiles/ByeByeBotCommands.html"));
 
 client.once('ready', () => {
   console.log('ByeByeBot Is Now Online');
@@ -28,7 +29,7 @@ client.on('message',async message =>{
     switch (command){
         case 'tellbyebye':
           //message.channel.send("The Bot commands can be found in the following attachment:");
-         // message.channel.send(botCommandsFile);
+         message.channel.send(botCommandsFile);
          const botInfo = new Discord.MessageEmbed()
          .setDescription('[The Bot commands can be found here.](http://bbbc.jtwp.org/)')
          message.channel.send(botInfo);
@@ -50,12 +51,13 @@ client.on('message',async message =>{
         case 'byebyebye':
           const connection = await message.member.voice.channel.join();
           connection.channel;
+          const dispatcher = connection.play(byeByeByeTrack);
           message.channel.send("You may hate me but it ain't no lie Baby bye bye bye");
           break;
         
           case 'kickbot':
-          const disconnect = await message.member.voice.channel.leave();
-          disconnect.channel;
+            await message.member.voice.channel.leave();
+            break; 
           
     }
 
@@ -76,24 +78,10 @@ client.on('message',async message =>{
          
           // User leaves a voice channel AND if it is not our bot with its id. 
           //console.log(oldMember +" has left the voice channel"); 
-          client.channels.cache.get('157705411422715905').send("<@"+newMember.member.user.id +">"+ ' Did you say bye to everyone?! "!Yes" or "!No".');  
+          client.channels.cache.get('157705411422715905').send("<@"+newMember.member.user.id +">"+ ' Did you say bye to everyone?!');  
          
-          // client.on('message', message =>{
-          //   const args = message.content.slice(prefix.length).split(/ +/);
-          //   const command = args.shift().toLowerCase();
-            
-          //   if(oldMember.channel)
-          //   {
-          //    if(command === 'yess') {message.channel.send("Okay! Goodbye! :grin:");}
-             
-          //   }
-          //   if(command === 'nno' && !newMember.channel)
-          //   {
-          //     message.channel.send("Well, get your ass back in there and say goodbye! :angry:"); 
-          //   }
-          // })
         }
 });
 
 
-client.login('DiscordToken');
+client.login('discordtoken');
